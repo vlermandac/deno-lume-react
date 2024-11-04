d1543017ba03abd1f95debf9230b16efd878d8b7
Vicente Lermanda <vlermanda@MacBook-Air-5.local>
2024-11-04 18:33:06 -0300

    b

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,12 @@
 import React from "npm:react";
 import Card from "./components/card.tsx";
-import BCard from "./components/bcard.tsx"
 export const title = "Welcome to my page";
 
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-950 text-white">
-    <BCard/>
+    <div className="flex relative h-screen items-center justify-center overflow-hidden h-full w-full bg-white">
+      <div className="absolute h-full w-full bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] [mask-image:radial-gradient(ellipse_50%_50%_at_50%_50%,#000_70%,transparent_100%)]"></div>
+      <Card/>
    </div> 
   );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,12 +1,14 @@
 import _React from "npm:react";
-import { Twitter, Instagram, Facebook } from "lucide-react"
+import { Linkedin, Github, Mail } from "lucide-react"
 
-export default function Component({
-  bannerUrl = "../assets/swing.jpeg",
-  avatarUrl = "../assets/lain.jpeg",
-  name = "A",
-  description = "some description",
-  socialLinks = { twitter: "#", instagram: "#", facebook: "#" }
+export default function Card({
+  name = "Vicente Lermanda",
+  description = "Software Engineer | CS MSc Student @ PUC",
+  socialLinks = { 
+    linkedin: "https://www.linkedin.com/in/vicente-lermanda-candia-18a779218/",
+    github: "https://github.com/vlermandac",
+    mail: "mailto:vicente@vlermanda.com"
+  }
 }) {
   return (
     <div className="card">
@@ -23,24 +25,35 @@ export default function Component({
         <rect width="100%" height="100%" filter="url(#noiseFilter)" opacity="0.14" clipPath="url(#rounded-clip)" />
       </svg>
 
-      <div className="banner relative">
-        <img src={bannerUrl} alt="banner" transform-images="avif 300@2 " className="w-full object-cover z-10" />
-        <div className="avatar absolute -bottom-16 left-1/2 h-32 w-32 -translate-x-1/2 overflow-hidden rounded-full border-4 border-[#2F2B48]">
-          <img src={avatarUrl} alt={name} transform-images="avif 300@2" className="h-full w-full object-cover z-10" />
-        </div>
-      </div>
-      <div className="content relative z-10 px-6 pb-6 pt-20 text-center">
+      <div className="content relative z-10 px-6 py-10 overflow-hidden">
         <h2 className="name mb-1 text-3xl font-bold">{name}</h2>
-        <p className="description mb-6 text-gray-300">{description}</p>
-        <div className="social-links flex justify-center space-x-4">
-          {socialLinks.twitter && (
-            <a href={socialLinks.twitter} className="text-gray-400 hover:text-white transition-colors" aria-label="Twitter">
-              <Twitter size={24} />
+        <p className="description mb-8 px-2 text-[#655E5B]">{description}</p>
+
+        <h2 className="name px-2 mt-1 text-xl font-bold">What do I do (mainly)</h2>
+        <p className="description mb-2 px-2 text-[#655E5B]">Web development, Data Analysis and MLops, Academic Research.</p>
+
+        <h2 className="name px-2 mt-4 text-xl font-bold">Currently working on</h2>
+        <ul className="list-disc list-inside mb-2 ">
+          <li><a href="https://nuestramemoria.ing.uc.cl/" className="text-[#907161] hover:text-[#655E5B]">Nuestra MemorIA</a></li>
+          <li><a href="https://ubicate.osuc.dev/" className="text-[#907161] hover:text-[#655E5B]">UbiCate</a></li>
+        </ul>
+        <h2 className="name px-2 mt-4 text-xl font-bold">Research Interests</h2>
+        <p className="description mb-4 px-2 text-[#655E5B] ">Graph Theory, Natural Language Processing, Graph Neural Networks, Algorithms and Data Structures, Model Expressiveness and Complexity Theory. </p>
+        
+        <div className="social-links justify-center flex space-x-8 mt-20 md:mt-5 xl:mt-10">
+          {socialLinks.linkedin && (
+            <a href={socialLinks.linkedin} target="_blank" rel="noopener noreferrer" className="text-[#907161] hover:text-[#655E5B] transition-colors" aria-label="Linkedin">
+              <Linkedin size={18} />
             </a>
           )}
-          {socialLinks.instagram && (
-            <a href={socialLinks.instagram} className="text-gray-400 hover:text-white transition-colors" aria-label="Instagram">
-              <Instagram size={24} />
+          {socialLinks.github && (
+            <a href={socialLinks.github} target="_blank" rel="noopener noreferrer" className="text-[#907161] hover:text-[#655E5B] transition-colors" aria-label="Github">
+              <Github size={18} />
+            </a>
+          )}
+          {socialLinks.mail && (
+            <a href={socialLinks.mail} target="_blank" rel="noopener noreferrer" className="text-[#907161] hover:text-[#655E5B] transition-colors" aria-label="Github">
+              <Mail size={18} />
             </a>
           )}
         </div>
